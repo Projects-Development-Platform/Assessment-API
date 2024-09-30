25e21f3187d55e0b8202207bc0f1f9ef02457b96
Manoj Neupane <75156544+Manpane@users.noreply.github.com>
2024-09-30 09:34:43 +0545

    port

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const YAML = require('yamljs');
 dotenv.config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const swaggerDocument = YAML.load('./swagger.yaml');
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
